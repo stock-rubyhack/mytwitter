902782e259271b140975d9fe65da6595d75a7a48
stock-rubyhack <stock99@debian9>
2020-07-05 11:57:19 +1000

    initial scaffolding code

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,18 @@
+# The controller for "Pages" which containing all of the code for any item inside of /pages
+class PagesController < ApplicationController
+  # back-end code for pages/index
+  def index
+  end
+
+  # back-end code for pages/home
+  def home
+  end
+
+  # back-end code for pages/profile
+  def profile
+  end
+
+  # back-end code for pages/explore
+  def explore
+  end
+end
